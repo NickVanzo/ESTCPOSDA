66aeb82172e07a584a3313c24f173bea59d0bc22
HallurKrist <halh@itu.dk>
2023-11-03 17:05:12 +0100

    fish collider added

---
+++ Exercise4/Engine/Engine/fishCollider.h
@@ -0,0 +1,22 @@
+#include "Component.h"
+
+namespace  MyEngine  {
+    class fishColliderComponent : public MyEngine::Component {
+
+    public:
+        bool getIsColliding();
+        void setIsColliding(bool _isColliding);
+
+        std::list<glm::vec2> getNodes();
+        void setNodes(std::list<glm::vec2> _nodes);
+
+        glm::vec2 getCenter();
+        void setCenter(glm::vec2 _center);
+
+    private:
+        bool isColliding = false;
+
+        std::list<glm::vec2> nodes = 0;
+        glm::vec2 center = glm::vec2(0, 1);
+    };
+}

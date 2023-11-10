d18a51c22055aeaf1248a67942a2d0e33a75c9e8
Tiago Fernandes <silvermoonstories@gmail.com>
2023-11-10 09:42:04 +0100

    Refactoring
    Refactoring of all old names

---
+++ FinalProject/Simulation/Game/GameManager/GameManager.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Nicolò Vanzo on 27/09/23.
+//
+#include "sre/SDLRenderer.hpp"
+#include "sre/SpriteAtlas.hpp"
+#include "Engine/MyEngine.h"
+#include "../Enums/FishSpawnerPositions.h"
+#include "GameManager.h"
+
+GameManager::~GameManager() {
+    std::cout << "Distruttore game manager" << std::endl;
+}
+void GameManager::StartGame() {
+    CreateFishSpawner();
+}
+
+void GameManager::CreateFishSpawner() {
+    MyEngine::Engine* engine = MyEngine::Engine::GetInstance();
+    lowerSpawner = engine->CreateGameObject("fishSpawner");
+    auto lowerSpawnerUpdateComponent = std::make_shared<Fishes::FishSpawner>(Fishes::LOWER, player, lowerSpawner);
+    lowerSpawner->AddComponent(lowerSpawnerUpdateComponent);
+
+    upperSpawner = engine->CreateGameObject("fishSpawner");
+    auto upperSpawnerUpdateComponent = std::make_shared<Fishes::FishSpawner>(Fishes::UPPER, player, upperSpawner);
+    upperSpawner->AddComponent(upperSpawnerUpdateComponent);
+}
